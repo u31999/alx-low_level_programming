ace8d9f9f73d53e2de5e8f3ff34b7f0ef5dc7c08
u31999 <u31999@gmail.com>
2022-06-02 16:22:00 -0700

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
+ *	*main - entry point
  *
- *  *main - entry point
  *
- *   *
+ *	*Return: 0 after printing the function
  *
- *    *Return: 0 after printing the function
- *
- *     */
+ **/
 
 int main(void)
 
