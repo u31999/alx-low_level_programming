d8753c2e59c92b5125c5da309bffdefcbf803fde
u31999 <u31999@gmail.com>
2022-06-03 10:35:45 -0700

    Print capital and small alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ **/
+
+int main(void)
+{
+	char ch;
+	char CH;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (CH = 'A'; CH <= 'Z'; CH++)
+	{
+		putchar(CH);
+	}
+
+	putchar('\n');
+	return (0);
+}
