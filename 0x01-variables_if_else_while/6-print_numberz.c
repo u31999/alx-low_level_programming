54fe5a668b0a2bf4f95b023d4cee071990bad173
u31999 <u31999@gmail.com>
2022-06-03 13:26:35 -0700

    Print number charcter

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Enty point
+ *
+ * Return: 0
+ *
+ **/
+
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
