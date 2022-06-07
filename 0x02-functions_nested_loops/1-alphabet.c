55d48f79cb3d3911110e4e81ed2b35ea0bc59fda
u31999 <u31999@gmail.com>
2022-06-07 10:46:48 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Enty point
+ * print_alphabet - print alphabet lowercase, return void
+ *
+ * Return: 0
+ **/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,13 @@ void put_char(void)
 {
 	printf("_putchar\n");
 }
+
+void print_alphabet(void)
+{
+	int letter;
+
+	for(letter = 'a'; letter <= 'z'; letter++){
+		putchar(letter);
+	}
+	putchar('\n');
+}
