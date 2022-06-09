1031bcc84299de562a27c53b0b11d5252f74651a
u31999 <u31999@gmail.com>
2022-06-09 12:28:47 -0700

    Prime factorial

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Enty point
+ * Return: Always 0
+ **/
+
+int main(void)
+{
+	long n = 612852475143;
+	int i;
+
+	for (i = 2; i < n; i++)
+	{
+		while (n % i == 0)
+			n = n / i;
+	}
+
+	printf("%lu\n", n);
+	return(0);
+}
