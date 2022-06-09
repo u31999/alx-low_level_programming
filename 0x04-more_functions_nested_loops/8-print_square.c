12e9a0d96de56963584cf51bbd3172e8d4a42636
u31999 <u31999@gmail.com>
2022-06-09 11:18:11 -0700

    Squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,22 +8,26 @@
 
 void print_square(int size)
 {
-	int i, j;
+	int i;
+	int k;
 
-	if (size <= 0)
-		_putchar('\n');
-	else
+	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
-		{
-			for (j = size; j > 0; j--)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-			_putchar('\n');
-		}
-		_putchar('\n');
-	}
+		{	
 
+			for (k = 0; k < size; k++)
+			{
+				_putchar (35);
+			}
+			_putchar ('\n');
+
+		}
+
+	}	
+
+	else
+	{
+		_putchar ('\n');
+	}
 }
