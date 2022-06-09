cc376e8b0cb7d9b4367ebbbdc3f42b5c1ae1907e
u31999 <u31999@gmail.com>
2022-06-08 17:06:55 -0700

    Multi line

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,17 @@
 #include "main.h"
-#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ */
+
 int main(void)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
+
 	positive_or_negative(i);
 
 	return (0);
+
 }
