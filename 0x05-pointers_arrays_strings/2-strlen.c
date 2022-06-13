c278f228c2442c7ed8194460d12c146e83cc15e5
u31999 <u31999@gmail.com>
2022-06-13 08:57:09 -0700

    Get the length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - Return the length of a string
+ * @*s: Pointer to string
+ * Return: Int
+ **/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while(s[i] != '\0')
+		i++;
+
+	return (i);
+}
