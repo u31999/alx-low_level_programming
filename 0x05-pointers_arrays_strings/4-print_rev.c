8eb03b687c125c6463c5726aa0320681e288fd0e
u31999 <u31999@gmail.com>
2022-06-13 09:46:19 -0700

    Print string reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - Print and reverse string
+ * @s: The string
+ * Return: void
+ **/
+
+void print_rev(char *s)
+{
+	int i = 0;
+	
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+
+	_putchar('\n');
+}
