61be28831fce78ac44dacfc959e25fa42a2bf74b
u31999 <u31999@gmail.com>
2022-06-13 13:05:56 -0700

    Fix betty style

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -23,13 +23,11 @@ int _atoi(char *s)
 	sign = 1;
 	number = 0;
 
-
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (is_numerical(s[i]))
 	{
-	number = (s[i] - 48) + number * 10;
+		number = (s[i] - 48) + number * 10;
 
 	if (s[i + 1] == ' ')
 		break;
@@ -41,7 +39,7 @@ int _atoi(char *s)
 
 	}
 
-	return (number *sign);
+	return (number * sign);
 
 
 }
