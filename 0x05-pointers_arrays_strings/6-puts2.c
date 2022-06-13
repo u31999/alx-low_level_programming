a19548c9790c1d05d1a2f964c40dd412d5d6ffbd
u31999 <u31999@gmail.com>
2022-06-13 12:44:31 -0700

    Fix the loop

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,18 +7,12 @@
 
 void puts2(char *str)
 {
-	int i = 0;
-	int j = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		i++;
-	}
-
-	while (j <= i)
-	{
-		_putchar(str[j]);
-		j += 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 
 	_putchar('\n');
