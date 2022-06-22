8ca8a785e2a7d1dbf188b942c8937f9cb53c274c
u31999 <u31999@gmail.com>
2022-06-22 15:16:41 -0700

    Mandatory tasks

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - Print out a string
+ * @s: The input
+ * Return: void
+ **/
+
+void _puts_recursion(char *s)
+{
+	if (*(s) != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*(s) == '\0')
+		_putchar('\n');
+}
